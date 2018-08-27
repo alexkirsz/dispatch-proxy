a99af2a7d679d7303556de7d7ef3bc459afd4e64
rlindsberg <roderick@karlemstrand.com>
2018-08-27 21:04:59 +0200

    import module os and list network interfaces

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import program from "commander";
 import Logger from "tmpl-log";
+import OS from "os";
 
 const logger = new Logger({ tab: 10, gutter: " " })
   .registerTag("b", ["bold"])
@@ -10,5 +11,13 @@ program.version(require("../package.json").version);
 program.command("echo <arg>").action(arg => {
   logger.emit("echo", `echoing "${arg}"`);
 });
+
+program.command('list')
+  .description('list all available network interfaces')
+  .action(function() {
+    console.log("listing interfaces:");
+    console.log(OS.networkInterfaces());
+  });
+
 
 program.parse(process.argv);
